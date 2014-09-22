0d7865d889262929717f0abf2af097ccf9f0221d
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-22 11:14:08 -0600

    completes reverse string without .reverse() method

---
+++ javascriptPractice.js
@@ -0,0 +1,16 @@
+// toy problems to practice fundamentals of JavaScript
+
+// write a function that reverses a string without using .reverse() method
+var string = 'this is a string'
+
+var revStr = function (str, newStr) {
+    str = str.split('');
+    newStr = newStr.split('');
+    for (var i = 0; i < str.length; i++) {
+      newStr.unshift(str[i]);
+    };
+    str = str.join('');
+    newStr = newStr.join('');
+    console.log(str);
+    console.log(newStr);
+}
