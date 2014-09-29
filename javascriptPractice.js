dd0639a33bff27698c2f44bb0a9043171635eb44
David Hugentobler <davidhugentobler30@gmail.com>
2014-09-29 10:35:59 -0600

    adds function that takes in a string, looks for "a", if "a" is there and "b" is exactly 3 places between them

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -62,6 +62,20 @@ var simpleSymbols = function (str) {
   return truthy;
 }
 
+// Have the function ABCheck(str) take the str parameter being passed and return the string true if the characters 'a' and 'b' are seperated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between 'a' and 'b'). Otherwise return the string false.
+
+var ABCheck = function (string) {
+    str = string.toLowerCase();
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] === 'a') {
+        if (str[i + 4] === 'b' || str[i - 4] === 'b') {
+          return true;
+        };
+      };
+    };
+  return false;
+};
+
 
 
 
