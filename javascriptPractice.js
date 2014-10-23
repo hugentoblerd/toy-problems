128740dab853ebac69f65d912c57fea55e90aa7d
David Hugentobler <davidhugentobler30@gmail.com>
2014-10-23 10:05:08 -0600

    adds this toy function "We are given 3 strings: str1, str2 and str3. Str3 is said to be a shuffle of str1 and str2 if it can be formed by interleaving the characters of str1 and str2 in a way that maintains the left to right ordering of the characters from each string. For example, given str1=abc and str2=def, str3=dabecf is a valid shuffle since it preserves the character ordering of the two strings. So, given these 3 strings write a function that detects whether str3 is a valid shuffle of str1 and str2."

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -247,6 +247,44 @@ var evenOccur1 = function (arr) {
   return 'There are no even occurences...'
 };
 
+// We are given 3 strings: str1, str2 and str3. Str3 is said to be a shuffle of str1 and str2 if it can be formed by interleaving the characters of str1 and str2 in a way that maintains the left to right ordering of the characters from each string. For example, given str1='abc' and str2='def', str3='dabecf' is a valid shuffle since it preserves the character ordering of the two strings. So, given these 3 strings write a function that detects whether str3 is a valid shuffle of str1 and str2.
+
+var interweave = function (str1, str2, str3) {
+  for (var i = 0; i < str3.length; i++) {
+    if (str1.indexOf(str3[i]) === -1 && str2.indexOf(str3[i]) === -1) {
+      return false;
+    }else if (str1.indexOf(str3[i]) !== -1) {
+      if (firstIndex && firstIndex > str1.indexOf(str3[i])) {
+        return false;
+      };
+      var firstIndex = str1.indexOf(str3[i]);
+    }else if (str2.indexOf(str3[i]) !== -1) {
+      if (secondIndex && secondIndex > str2.indexOf(str3[i])) {
+        return false;
+      };
+      var secondIndex = str2.indexOf(str3[i]);
+    };
+  };
+  return true;
+};
+
+var interweave2 = function (str1, str2, str3) {
+  var newStr1 = '';
+  var newStr2 = '';
+  for (var i = 0; i < str3.length; i++) {
+    if (str1.indexOf(str3[i]) !== -1) {
+      newStr1 += str3[i];
+    }else {
+      newStr2 += str3[i];
+    }
+  };
+  if (newStr1 === str1 && newStr2 === str2) {
+    return true;
+  }else {
+    return false;
+  }
+};
+
 
 
 
